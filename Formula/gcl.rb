a399817c97ac720e693ab801864c2b9d8f1be39a
Camm Maguire <camm@transcendence.maguirefamily.org>
2026-05-15 18:31:23 -0400

    try homebrew2

--- Formula/gcl.rb
+++ Formula/gcl.rb
@@ -3,8 +3,8 @@ class Gcl < Formula
   homepage "https://gnu.org/software/gcl"
   # Pull directly from the upstream GNU Savannah repository
   url "git://git.sv.gnu.org/gcl.git",
-      tag:      "Version_2_7_2pre_homebrew1", # Replace with your target version tag
-      revision: "75a48718e9b750df5a62ad21b41dbfe3cbed93cf" # Replace with the exact Git commit hash
+      tag:      "Version_2_7_2pre_homebrew2", # Replace with your target version tag
+      revision: "3fc457ca07c17625cc5772caebeacbfca26d8a6a" # Replace with the exact Git commit hash
   license "GPL-2.0-or-later"
 
   # Force Homebrew to drop its sandboxed shims and use the standard shell environment paths
