431d6cc81b371d50e041b7d65a356cf61cef13b4
Camm Maguire <camm@transcendence.maguirefamily.org>
2026-06-01 08:51:28 -0400

    killall 2

--- Formula/acl2-gcl.rb
+++ Formula/acl2-gcl.rb
@@ -70,10 +70,7 @@ class Acl2Gcl < Formula
     if ENV["HOMEBREW_ACL2_BUILD"] == "books"
       system <<~SHELL
            tar zxf $HOMEBREW_ACL2_ICF
-           rm -f debian/test.log
-           gmake -O -f debian/rules debian/test.log & j=\$! ; (sleep 300; cat debian/test.log >>debian/test.log.all; kill \$j; killall gmake) & wait \$j
-           [ -e debian/saved_acl2 ] || mv debian/saved_acl2.ori debian/saved_acl2
-           touch infix-stamp build-stamp
+           gmake -O -f debian/rules build & j=\$! ; (sleep 300; ! [ -e saved_acl2.ori ] || (cat debian/test.log >>debian/test.log.all; kill -\$j; rm -f debian/test.log; mv saved_acl2.ori saved_acl2) & k=\$!; ! wait \$j || kill \$k
            mkdir -p #{prefix}
            tar zcf #{prefix}/$HOMEBREW_ACL2_OCF .
       SHELL
