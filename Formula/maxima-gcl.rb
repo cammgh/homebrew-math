470a282b99024d3b963e569e9fa62b205a8191d5
Camm Maguire <camm@transcendence.maguirefamily.org>
2026-06-11 15:47:21 -0400

    escape

--- Formula/maxima-gcl.rb
+++ Formula/maxima-gcl.rb
@@ -51,9 +51,9 @@ class MaximaGcl < Formula
     system <<~SHELL
            mkdir bin || true
            echo "(progn (setq si::*optimize-maximum-pages* nil)(when (fboundp (quote si::sgc-on)) (fmakunbound (quote si::sgc-on)))(si::save-system \\"bin/gcl\\"))" | gcl
-           ./configure --prefix=#{prefix} --enable-gcl --with-gcl=\\$(pwd)/bin/gcl
+           ./configure --prefix=#{prefix} --enable-gcl --with-gcl=$(pwd)/bin/gcl
            make
-           echo ":lisp (setq maxima::*maxima-started* nil si::*optimize-maximum-pages* t si::*readline-prefix* \\"MAXIMA::\\$\\$\\" si::*allow-gzipped-file* t)(si::gbc t)(si::save-system \\"foo\\")" | ./maxima-local && mv foo src/binary-gcl/maxima
+           echo ":lisp (setq maxima::*maxima-started* nil si::*optimize-maximum-pages* t si::*readline-prefix* \\"MAXIMA::\$\$\\" si::*allow-gzipped-file* t)(si::gbc t)(si::save-system \\"foo\\")" | ./maxima-local && mv foo src/binary-gcl/maxima
            make install
     SHELL
   end
