b99245c678a29c1dc22806dc963e38038ff1af04
Camm Maguire <camm@transcendence.maguirefamily.org>
2026-06-14 17:11:08 -0400

    homebrew 24

--- Formula/gcl27.rb
+++ Formula/gcl27.rb
@@ -2,9 +2,9 @@ class Gcl27 < Formula
   desc "GNU Common Lisp"
   homepage "https://gnu.org/software/gcl"
   url "git://git.sv.gnu.org/gcl.git",
-      tag:      "Version_2_7_2pre_homebrew23", # Replace with your target version tag
-      revision: "38430e726e850c807445b4e44d78d988b60d635f" # Replace with the exact Git commit hash
-  version "2.7.2prehb23"
+      tag:      "Version_2_7_2pre_homebrew24", # Replace with your target version tag
+      revision: "dc0d83d13a255e6a37cdef4b8b57878ac2065754" # Replace with the exact Git commit hash
+  version "2.7.2prehb24"
   license "GPL-2.0-or-later"
 
   bottle do
