b8064a4bb5fa1e41b96829fd1105ae68ae67ca61
Camm Maguire <camm@transcendence.maguirefamily.org>
2026-06-14 09:02:41 -0400

    p2

--- Formula/maxima-gcl.rb
+++ Formula/maxima-gcl.rb
@@ -97,12 +97,12 @@ __END__
 --- ./doc/info/build_index.pl1  2026-06-14 12:54:18
 +++ ./doc/info/build_index.pl   2026-06-14 12:54:56
 @@ -252,7 +252,7 @@
- 
+
  #        Construct hashtables from the lists given above.
- 
+
 -print "(load-info-hashtables (maxima::maxima-load-pathname-directory) deffn-defvr-pairs section-pairs))\n";
 +print "(clrhash cl-info::*info-tables*)(load-info-hashtables (maxima::maxima-load-pathname-directory) deffn-defvr-pa\
 irs section-pairs))\n";
- 
+
  # (2.3)  Do we have any items or sections?
  #
